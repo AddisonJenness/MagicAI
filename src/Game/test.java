f242dc985442605906ad2d78103b7f3a3d6d7cf1
or9215qv <or9215qv@or9215qv71.workstations.winona.edu>
2017-11-29 19:49:03 -0600

    Made test class please ignore

---
+++ src/Game/test.java
@@ -0,0 +1,17 @@
+package Game;
+
+import jess.Jesp;
+import jess.Rete;
+import jess.WorkingMemoryMarker;
+
+public class test {
+
+	private Rete engine;
+	private WorkingMemoryMarker marker;
+	private Database database;
+
+
+public void Usecase1 () {
+	
+}
+}
--- src/UI/App.java
+++ src/UI/App.java
@@ -12,7 +12,7 @@ public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		Player p1 = new Player();
 		Player p2 = new Player();
 		ArrayList<Card> p1d = new ArrayList<Card>();
@@ -30,7 +30,7 @@ public class App {
 		Card LargerThan = new Card("LargerThan",Type.SORCERY, 0, 0, 2, Ability.PUMP);
 		Card Explore = new Card("Explore",Type.SORCERY, 0, 0, 2, Ability.EXPLORE);
 		Card PreyUpon = new Card("PreyUpon",Type.SORCERY, 0, 0, 1, Ability.FIGHT);
-		
+
 		//p2 Cards
 		Card Mountain = new Card("Mountain", Type.LAND, 0,0,0,null);
 		Card Falkenrath = new Card("Falkenrath Reaver",Type.CREATURE, 2, 2, 2, null);
@@ -43,51 +43,81 @@ public class App {
 		Card Arrow = new Card("Arrow Storm", Type.SORCERY,0,0,5,Ability.BURN);
 		Card Pillar = new Card("Pillar of Flame",Type.SORCERY, 0, 0, 1, Ability.BURN);
 		Card IncinFlow = new Card("Incendiary Flow", Type.SORCERY, 0, 0, 2, Ability.BURN);
-		
-		
+
+
 		//2
 		for(int i=0; i<2; i++) {
-		p1d.add(TuskedColos);
-		p1d.add(PreyUpon);
-		p1d.add(Thornhide);
-		p1d.add(FeralKrus);
-		p1d.add(Colossodon);
-		p1d.add(AlpineGrizz);
-		p1d.add(CentaurCour);
+			p1d.add(TuskedColos);
+			p1d.add(PreyUpon);
+			p1d.add(Thornhide);
+			p1d.add(FeralKrus);
+			p1d.add(Colossodon);
+			p1d.add(AlpineGrizz);
+			p1d.add(CentaurCour);
 		}
 		//4
 		for(int i=0; i<4; i++) {
-		p1d.add(LargerThan);
-		p1d.add(Explore);
-		p1d.add(TerainEle);
-		p1d.add(Broodhunter);
+			p1d.add(LargerThan);
+			p1d.add(Explore);
+			p1d.add(TerainEle);
+			p1d.add(Broodhunter);
 		}
 		for(int i=0; i<20; i++) {
-		p1d.add(Forest);	
+			p1d.add(Forest);	
 		}
-		System.out.println(TerainEle.toString());
-		
+		//2
+		for(int i=0; i<2; i++) {
+			p2d.add(Hulking);
+			p2d.add(Hyena);
+			p2d.add(Blazing);
+		}
+		//4
+		for(int i=0; i<4; i++) {
+			p2d.add(Earth);
+			p2d.add(Spitting);
+			p2d.add(Frendied);
+			p2d.add(Falkenrath);
+			p2d.add(Arrow);
+			p2d.add(IncinFlow);
+			p2d.add(Pillar);
+
+		}
+		for(int i=0; i<20; i++) {
+			p2d.add(Mountain);	
+		}
+
+		//System.out.println(TerainEle.toString());
+		p2.setDeck(p2d);
 		p1.setDeck(p1d);
 		Random rnd = new Random();
 		ArrayList<Card> hand = new ArrayList<Card>();
 		for(int i=0; i<7; i++) {
-		int e=rnd.nextInt(p1.getDeck().size());
-		Card c = p1.getDeck().get(e);
-		p1.getDeck().remove(e);
-		hand.add(c);
-		System.out.println(i);
-		p1.setHand(hand);
+			int e=rnd.nextInt(p1.getDeck().size());
+			Card c = p1.getDeck().get(e);
+			p1.getDeck().remove(e);
+			hand.add(c);
+			//System.out.println(i);
+			p1.setHand(hand);
 		}
 		System.out.println("Player 1 Hand");
 		for(int i=0; i<p1.getHand().size(); i++) {
-		
-		System.out.println(p1.getHand().get(i).toString()+"\n");
+
+			System.out.println(p1.getHand().get(i).toString()+"\n");
+		}
+		hand.clear();
+		for(int i=0; i<7; i++) {
+			int e=rnd.nextInt(p2.getDeck().size());
+			Card c = p2.getDeck().get(e);
+			p2.getDeck().remove(e);
+			hand.add(c);
+			//System.out.println(i);
+			p2.setHand(hand);
 		}
 		System.out.println("Player 2 Hand");
-		//for(int i=0; i<p2.getHand().size(); i++) {
-			
-	//	}
-		
+		for(int i=0; i<p2.getHand().size(); i++) {
+			System.out.println(p2.getHand().get(i).toString()+"\n");
 		}
+
+	}
 
 }
